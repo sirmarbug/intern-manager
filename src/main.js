e761a7a5d4e5fedf33a255f7dcdeab40f7bd67d7
Mariusz Bugajski <mbugajski.dev@gmail.com>
2020-01-20 17:29:07 +0100

    IM: Implemented logger

--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import router from './router';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
+import VueLogger from 'vuejs-logger';
+const isProduction = process.env.NODE_ENV === 'production';
+ 
 Vue.config.productionTip = false;
+
+const options = {
+    isEnabled: true,
+    logLevel : isProduction ? 'error' : 'debug',
+    stringifyArguments : false,
+    showLogLevel : true,
+    showMethodName : true,
+    separator: '|',
+    showConsoleColors: true
+};
+
+Vue.use(VueLogger, options);
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
